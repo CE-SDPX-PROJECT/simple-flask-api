15e2eb62dda22229386e3302a8180f3674826011
Prataad <pondaeroz008y@gmail.com>
2025-02-05 12:52:55 +0700

    feat: rafactor app

--- api/app.py
+++ api/app.py
@@ -1,42 +1,84 @@
 from flask import Flask, request, jsonify
-from db import get_all, get_by_id, insert, update, delete
+import os
+from user import (validate_user_input as validate_user_input_service,
+                  add_user as add_user_service,
+                  update_user as update_user_service,
+                  delete_user as delete_user_service,
+                  get_all_users as get_all_users_service,
+                  get_user_by_id as get_user_service)
+
+
 from dotenv import load_dotenv
-from utils import convert_users_to_dict, convert_user_to_dict
+
+env_file = os.getenv('ENV_FILE', '.env.dev')
+load_dotenv(env_file)
+
+print(f"ENV_FILE: {env_file}")
 
 app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health():
-    print('Health check')
-    return jsonify({'status': 'healthy'})
+    return jsonify({'response': 'ok'})
 
-@app.route('/users', methods=['GET'])
+@app.route('/user', methods=['GET'])
 def get_users():
-    try:
-        users = get_all('USERS')
-        return jsonify(convert_users_to_dict(users)), 200
-    except Exception as error:
-        print(f'Error getting users: {error}')
-        return jsonify({'error': 'Internal Server Error'}), 500
+    users, error, status_code = get_all_users_service()
+    if error:
+        return jsonify({'message': f'Error retrieving users: {error}'}), status_code
+    return jsonify(users)
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-    try:
-        user = get_by_id('USERS', user_id)
-        if not user:
-            return jsonify({'error': 'User not found'}), 404
-        return jsonify(convert_user_to_dict(user)), 200
-    except Exception as error:
-        print(f'Error getting user: {error}')
-        return jsonify({'error': 'Internal Server Error'}), 500
+@app.route('/user/<int:uid>', methods=['GET'])
+def get_user(uid):
+    user, error, status_code = get_user_service(uid)
+    if error:
+        return jsonify({'message': error}), status_code
+    return jsonify(user)
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    try:
-        data = request.
-    except:
-        return jsonify({'error': 'Internal Server Error'}), 500
+@app.route('/user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    
+    error_message, valid, status_code = validate_user_input_service(data)
+    if not valid:
+        return jsonify({'message': error_message}), status_code
+
+    name = data.get('name')
+    age = data.get('age')
+
+    response_data, error, status_code = add_user_service(name, age)
+    if error:
+        return jsonify({'message': f'Error adding user: {error}'}), status_code
+    return jsonify(response_data), status_code
+
+@app.route('/user/<int:uid>', methods=['PATCH'])
+def update_user(uid):
+    data = request.get_json()
+    
+    user, error, status_code = get_user_service(uid)
+    if error:
+        return jsonify({'message': error}), status_code
+
+
+    response_data, error, status_code = update_user_service(uid, name=data.get('name'), age=data.get('age'))
+    if error:
+        return jsonify({'message': f'Error updating user: {error}'}), status_code
+    else:
+        return jsonify(response_data), status_code
+
+@app.route('/user/<int:uid>', methods=['DELETE'])
+def delete_user(uid):
+    user, error, status_code = get_user_service(uid)
+    if error:
+        return jsonify({'message': error}), status_code
+
+    response_data, error, status_code = delete_user_service(uid)
+    if error:
+        return jsonify({'message': f'Error deleting user: {error}'}), status_code
+    return jsonify(response_data), status_code
 
 if __name__ == '__main__':
-    load_dotenv()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv('FLASK_PORT', 5000))
+    print("port", port)
+    env = os.getenv('FLASK_ENV', 'development')
+    app.run(host='0.0.0.0', port=port, debug=env == 'development')
